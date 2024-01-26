4f2adb3317d1b36eae78689bde4a82eb620a7d2c
unknown <jritsqmet@gmail.com>
2024-01-25 22:43:25 -0500

    actualización

--- build_and_run.bat
+++ build_and_run.bat
@@ -2,7 +2,7 @@
 
 
 :: Instalar las dependencias necesarias
-C:\ProgramData\Jenkins\.jenkins\workspace\test2\venv\Scripts\pip install -r requirements.txt
+::C:\ProgramData\Jenkins\.jenkins\workspace\test2\venv\Scripts\pip install -r requirements.txt
 
 :: Ejecutar la aplicación de Streamlit
 streamlit run streamlit_app.py
